5935b5dc0ecad687401d30e6b4fad3700318711f
Marshall <romane.marshall@accenture.com>
2023-04-12 13:22:34 -0400

    first commit

---
+++ NBA/src/test/java/com/tracker/NBA/NbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.NBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
